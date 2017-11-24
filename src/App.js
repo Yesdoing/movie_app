450f82cccf213334c84776f6642d93ec9782df55
Jane Lee <Yesdoing13@gmail.com>
2017-11-24 22:49:43 +0900

    change url

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ _getMovies = async () => {
 }
 
 _callApi = () => {
-  return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating')
+  return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
   .then(response => response.json())
   .then(json => json.data.movies)
   .catch(err => console.log(err))
